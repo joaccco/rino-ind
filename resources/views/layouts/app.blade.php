8d6751869f9d4a59c0a9447bbfcc9eb713a68840
joaccco <joaccofranc02@icloud.com>
2024-05-10 05:05:45 -0300

    Configuracion de Base de datos y creacion de vistas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,21 +21,14 @@
         <x-banner />
 
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+            @livewire('navigation')
 
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
+
+            @livewire('footer')
         </div>
 
         @stack('modals')
--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('home.index');
+});
+
+Route::get('/contact', function () {
+    return view('contact.index');
+});
+
+Route::get('/blog', function () {
+    return view('blog.index');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('welcome');
     })->name('dashboard');
 });
